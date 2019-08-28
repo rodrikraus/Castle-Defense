7511101251eaa98537323057b94b1b3ab4a07286
rodrikraus <54340315+rodrikraus@users.noreply.github.com>
2019-08-28 00:12:25 -0300

    Commit test.

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,5 +3,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Chau mundo");
+		System.out.println("river>boca");
+		System.out.println("ésta linea fue agregada por mati");
+		System.out.println("ésta linea fue agregada por juani");
 	}
 }
