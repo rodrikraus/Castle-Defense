9484f4673b06b9b2e0d3cef098b3775f5e5ae1eb
Matías David Schwerdt <schwerdt.matias@gmail.com>
2019-09-07 10:31:43 -0300

    no c bro jaja

--- src/GUI.java
+++ src/GUI.java
@@ -7,20 +7,17 @@ import javax.swing.border.EmptyBorder;
 
 public class GUI extends JFrame {
 	
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JLabel dibujo;
 	
 	public GUI() {
-		getContentPane().setLayout(null);
-		
+		getContentPane().setLayout(null);		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
+		contentPane.setLayout(null);		
 		this.agregarDibujo();
 	}
 	
