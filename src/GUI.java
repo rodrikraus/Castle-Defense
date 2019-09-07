66a70c57cb6cb3352c2a1a8002c83ab1be81b95d
rodrikraus <54340315+rodrikraus@users.noreply.github.com>
2019-09-07 20:25:43 -0300

    agregue el fondo

--- src/GUI.java
+++ src/GUI.java
@@ -7,18 +7,21 @@ import javax.swing.border.EmptyBorder;
 
 public class GUI extends JFrame {
 	
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JLabel dibujo;
+	private JLabel dibujo, fondo;
 	
 	public GUI() {
-		getContentPane().setLayout(null);		
+		getContentPane().setLayout(null);	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 544, 349);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);		
+		contentPane.setLayout(null);
+		
 		this.agregarDibujo();
+		this.agregarFondo();
 	}
 	
 	public void moverse() {
@@ -34,15 +37,22 @@ public class GUI extends JFrame {
 			dibujo.setBounds(newX, newY, ancho, alto);
 			
 			try {
-				Thread.sleep(5); //Esto seria la velocidad a la que corre el juego
+				Thread.sleep(5);
 			} catch (InterruptedException e) {}
 		}
 	}
 	
-	private void agregarDibujo(){
+	private void agregarDibujo() {
 		ImageIcon imagen = new ImageIcon(this.getClass().getResource("img/perro.gif"));
 		dibujo = new JLabel(imagen);
-		dibujo.setBounds(350, 120, 50, 50);
+		dibujo.setBounds(544, 150, 62, 38);
 		this.add(dibujo);
+	}
+	
+	private void agregarFondo() {
+		ImageIcon imagen = new ImageIcon(this.getClass().getResource("img/fondo.png"));
+		fondo = new JLabel(imagen);
+		fondo.setBounds(1, 1, 544, 349);
+		this.add(fondo);
 	}
 }
