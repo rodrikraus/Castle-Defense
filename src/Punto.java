6302d858a67b29379234da99aa05271874170440
Matías David Schwerdt <schwerdt.matias@gmail.com>
2019-09-13 21:26:11 -0300

    Parte gráfica del GameObject

--- src/Punto.java
+++ src/Punto.java
@@ -1,25 +1,41 @@
-import java.awt.Rectangle;
 
-public class Punto extends Rectangle {
+public class Punto {
 
-	private static final long serialVersionUID = 1L;
-	protected double x;
-	protected double y;
+	protected int x;
+	protected int y;
+	protected int ancho;
+	protected int largo;
 		
-		
+
 	public Punto(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+		
+	public int getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+
+	public int getLargo() {
+		return largo;
+	}
+
+	public void setLargo(int largo) {
+		this.largo = largo;
+	}
 	
-	public double getX() {
+	public int getX() {
 		return x;
 	}
 	public void setX(int x) {
 		this.x = x;
 	}
 	
-	public double getY() {
+	public int getY() {
 		return y;
 	}
 	
