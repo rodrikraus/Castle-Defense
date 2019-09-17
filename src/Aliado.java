895d6df2514a394a4c78374d851d1156b922280a
juanifabrega <juanifabrega1997@gmail.com>
2019-09-16 23:54:35 -0300

    set y get de costo

--- src/Aliado.java
+++ src/Aliado.java
@@ -2,4 +2,13 @@
 public abstract class Aliado extends Personaje {
 	
 	protected int costo;
+	
+	public void setCosto(int c) {
+		costo=c;
+	}
+	
+	public int getCosto() {
+		return costo;
+	}
+	
 }
