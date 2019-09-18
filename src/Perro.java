32169c1dd32ffcc370da6a044c77096b411ab1ab
juanifabrega <juanifabrega1997@gmail.com>
2019-09-18 09:50:07 -0300

    actualizacion

--- src/Perro.java
+++ src/Perro.java
@@ -7,21 +7,18 @@ public class Perro extends Enemigo {
 	private final int largo = 38;
 	
 	public Perro(Punto p) {
-		rango = 1;
-		daño = 10;
 		vida = 30;
-//		velocidad = ¿RAPIDA?
-		puntos = 35;
-		monedas = 30;
-		
-		
 		ubicacion=p;
 		ImageIcon imagen = new ImageIcon(this.getClass().getResource("enemigos/perro.gif"));
 		dibujo = new JLabel(imagen);
 		//dibujo.setBounds(p.getX(), p.getY(), ancho, largo);
+		rango = 1;
+		danio = 10;
+		//velocidad = ¿RAPIDA?
+		puntos = 35;
+		monedas = 30;		
 	}
 	
-
 	public int getAncho() {
 		return ancho;
 	}
