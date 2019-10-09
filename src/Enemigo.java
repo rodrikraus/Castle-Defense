c80b516435c1f80be1c5f1f93a9642805172b9ca
Mati Chewer <schwerdt.matias@gmail.com>
2019-10-08 21:40:50 -0300

    probando commits con nueva pc

--- src/Enemigo.java
+++ src/Enemigo.java
@@ -3,7 +3,7 @@ public abstract class Enemigo extends Personaje {
 	
 	protected int velocidad;
 	protected int puntos;
-	protected int monedas;
+	protected int monedas; 
 	
 	public abstract void interactuar();
 	
