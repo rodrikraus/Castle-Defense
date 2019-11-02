87b8ec01cd47aa8d2a0b4c685e845c529253283d
matichewer <schwerdt.matias@gmail.com>
2019-11-02 19:41:38 -0300

    saqué la Ñ del nombre de un archivo

--- src/GameObjects/DobleDanio.java
+++ src/GameObjects/DobleDanio.java
@@ -9,11 +9,13 @@ public class DobleDanio extends Premio {
 
 	public DobleDanio(Punto p) {
 		super(1,0,0,0);
-		punto=p;
+		punto = p;
 		visitor= new VisitorPremio(this);
-		ruta_dibujo_quieto = "img/mapa/powerupDobleDaño.gif";
+		
+		ruta_dibujo_quieto = "img/mapa/powerupDobleDanio.gif";
 		ruta_dibujo_moviendose = null;
 		ruta_dibujo_ataque = null;
+		
 		cambiarDibujo(ruta_dibujo_quieto);
 
 	}
