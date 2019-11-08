9a9ca6e860fb05e566153c3fe02b8d476278c1ce
rodrikraus <54340315+rodrikraus@users.noreply.github.com>
2019-11-08 00:39:12 -0300

    Algoritmo de oleadas refinado.

--- src/Juego/Test2.java
+++ src/Juego/Test2.java
@@ -4,11 +4,29 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		
-		String oleadas = "ddpjm mvbppp";
+		String oleadas = "1ddpjm2mvbppp3"; //1, 2 y 3 indican las diferentes oleadas
 		int tiempo = 1000;  //lo pongo aca asi es mas facil de cambiar
 		
 		for(int i = 0; i < oleadas.length(); i++) {
 			switch(oleadas.charAt(i)) {
+				case '1': {
+					System.out.println("Oleada 1 comenzando.");
+					try {Thread.sleep(tiempo);} catch (InterruptedException e) {e.printStackTrace();}
+					break;
+				}
+				
+				case '2': {
+					System.out.println("Oleada 2 comenzando.");
+					try {Thread.sleep(tiempo);} catch (InterruptedException e) {e.printStackTrace();}
+					break;
+				}
+				
+				case '3': {
+					System.out.println("Oleada 3 comenzando.");
+					try {Thread.sleep(tiempo);} catch (InterruptedException e) {e.printStackTrace();}
+					break;
+				}
+				
 				case 'p': {
 					System.out.println("Creando perro.");
 					try {Thread.sleep(tiempo);} catch (InterruptedException e) {e.printStackTrace();}
@@ -40,12 +58,6 @@ public class Test2 {
 				
 				case 'v': {
 					System.out.println("Creando vampira.");
-					try {Thread.sleep(tiempo);} catch (InterruptedException e) {e.printStackTrace();}
-					break;
-				}
-				
-				case ' ': {
-					System.out.println("Cambiando de oleada.");
 					try {Thread.sleep(tiempo);} catch (InterruptedException e) {e.printStackTrace();}
 					break;
 				}
